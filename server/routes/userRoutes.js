8cf1d0feb44dfaf6440a675d7edd8b67ffa20237
Venkat-rgb <venkymandadi62@gmail.com>
2024-08-05 21:56:20 +0530

    Done code changes in userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,8 +11,10 @@ const router = express.Router();
 // verify token is used to check whether user is logged in.
 router.get("/me", verifyToken, getUser);
 
+// Updates the logged in user's profile
 router.put("/me/update", verifyToken, updateUser);
 
+// Updates the logged in user's password
 router.put("/me/password/update", verifyToken, updateMyPassword);
 
 export default router;
