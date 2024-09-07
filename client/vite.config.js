4efc92822b5c26815c5a883693cc2c15c595a8fd
Venkat-rgb <venkymandadi62@gmail.com>
2024-09-07 23:35:35 +0530

    Done changes in vite.config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import compression from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
+// Adding both gzip and brotli compression
 export default defineConfig({
   plugins: [
     react(),
