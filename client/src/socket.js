745dc38568679b04197d9570f514a49b6c9b451a
Venkat-rgb <venkymandadi62@gmail.com>
2024-09-08 21:53:46 +0530

    Done code changes in socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,4 +2,5 @@ import { io } from "socket.io-client";
 
 const ENDPOINT = import.meta.env.VITE_APP_SOCKET_BACKEND_URL;
 
+// Initializing socket connection from frontend to socket server
 export const socket = io(ENDPOINT, { autoConnect: false });
